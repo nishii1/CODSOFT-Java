58cffc45cea25d05dec4c5f52553d546c0957f6e
Nishi <141473928+nishii1@users.noreply.github.com>
2023-08-11 17:42:34 +0530

    add

---
+++ Currency converter.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.text.DecimalFormat;
+ 
+class CurrencyConverter {
+      
+    public static void main(String[] args) {
+  
+        double amount, dollar, code, ringgit ;
+  
+        DecimalFormat f = new DecimalFormat("##.##");
+  
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+  
+        System.out.println("USD & MYR Currency Conventer");
+  
+        System.out.println("Which currency You want to Convert ? ");
+         
+        System.out.println("1:Dollar \t2:Ringgit ");
+        code = sc.nextInt();
+         
+        System.out.println("How much Money you want to convert ?");
+        amount = sc.nextFloat();
+  
+        // For amounts Conversion
+        if (code == 1) {
+  
+            // For Dollar Conversion
+  
+            ringgit = amount * 4.17;
+            System.out.println("Your " + amount + " Dollar is : " + f.format(ringgit) + " ringgit");
+ 
+        } else if (code == 2) {
+            // For Dollar Conversion
+  
+            ringgit = amount * 4.17;
+            System.out.println("Your " + amount + " Dollar is : " + f.format(ringgit) + " ringgit");
+ 
+  
+        } else {
+            System.out.println("Invalid input");
+        }
+    }
+}
+         
